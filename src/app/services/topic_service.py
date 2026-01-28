fe207f2868147a0c1cae99a252bd0ae8d94bd797
Simon Okutan <simon.spam16@gmail.com>
2026-01-28 20:58:17 +0100

    Make chunck sizes larger to reduce costs

--- src/app/services/topic_service.py
+++ src/app/services/topic_service.py
@@ -85,17 +85,17 @@ async def run_topic_analysis_printed_paper_pipeline(
             Pipeline.init(
                 input_data={"text": text}, pipeline_config=PipelineConfig(name="Printed Paper Topic Analysis Pipeline")
             )
-            .exec(SpeechSplitter(config=SpeechSplitterConfig()))
+            .exec(SpeechSplitter(config=SpeechSplitterConfig(chunk_size=10000, chunk_overlap=200)))
             .exec(TopicExtractor(config=TopicExtractorConfig(inject_topics=[title])))
             .exec(
                 TopicEmbedder(config=TopicEmbedderConfig(embedding_model_name="sentence-transformers/all-MiniLM-L6-v2"))
             )
-            .exec(ClusterTopics(config=ClusterTopicsConfig(eps=0.25, min_samples=1)))
+            .exec(ClusterTopics(config=ClusterTopicsConfig(eps=0.5, min_samples=1)))
             .exec(ClusterRepresentativePicker(config=None))
             .exec(
                 TopicEmbedder(config=TopicEmbedderConfig(embedding_model_name="sentence-transformers/all-MiniLM-L6-v2"))
             )
-            .exec(ClusterTopicsGlobal(config=ClusterTopicsConfig(eps=0.25, min_samples=1), session=session))
+            .exec(ClusterTopicsGlobal(config=ClusterTopicsConfig(eps=0.5, min_samples=1), session=session))
             .exec(Formatter(config=FormatterConfig(formatter_function=formatter_final)))
             .compile()
         )
