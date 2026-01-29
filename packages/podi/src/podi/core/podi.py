a6e8285ed21e7e2dd46437460dc4ef357e3c73b2
Samuel Sacher <sam02de-dev@outlook.com>
2026-01-29 19:35:23 +0100

    Updated acceptable ranges for activity length

--- packages/podi/src/podi/core/podi.py
+++ packages/podi/src/podi/core/podi.py
@@ -79,8 +79,8 @@ async def find_activites_stmt(key, length, settings) -> GenerativeSelect:
                     a1.text.is_not(None),
                     ~a1.text.in_(["", "[NoTextAvailable]"]),
                     prot.date >= datetime.datetime.fromisoformat(settings.PROCESS_START_DATE).date(),
-                    func.length(a1.text) > 1000,
-                    func.length(a1.text) < 100000,
+                    func.length(a1.text) > 50,
+                    func.length(a1.text) < 50000,
                 ),
                 and_(
                     a1.document_type == "printedPaper",
